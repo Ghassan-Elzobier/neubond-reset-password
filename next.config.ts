7c140e656b909d01733298210abbc760674429f2
Ghassan-Elzobier <ghassanelzobier@gmail.com>
2026-03-10 16:00:08 +0000

    headers for AASA

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,20 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
 };
 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
+};
+
 export default nextConfig;
