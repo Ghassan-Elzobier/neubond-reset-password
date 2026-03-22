cf166e1f038b6ac72f3cad0d39b2ed8239ac7e36
Ghassan-Elzobier <ghassanelzobier@gmail.com>
2026-03-22 15:08:26 +0000

    Force dynamic rendering

--- app/auth/update-password/page.tsx
+++ app/auth/update-password/page.tsx
@@ -1,4 +1,6 @@
-"use client";
+export const dynamic = "force-dynamic";
+
+("use client");
 
 import { useSearchParams } from "next/navigation";
 import { UpdatePasswordForm } from "@/components/update-password-form";
