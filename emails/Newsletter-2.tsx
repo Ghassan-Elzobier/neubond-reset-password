7c6c890f4638c266fa9702f8541873bcb9083c59
Ghassan-Elzobier <ghassanelzobier@gmail.com>
2026-04-30 12:12:04 +0100

    Init Neubond Newsletter 2

---
+++ emails/Newsletter-2.tsx
@@ -0,0 +1,58 @@
+import { Body, Container, Head, Html, Preview } from "@react-email/components";
+
+import TopBar from "./components/TopBar";
+import Logo from "./components/Logo";
+import Hero from "./components/Hero";
+import VisionSection from "./components/VisionSection";
+import WorkingOnSection from "./components/WorkingOnSection";
+import LookingAheadSection from "./components/LookingAheadSection";
+import SectionImage from "./components/SectionImage";
+import SocialBlock from "./components/SocialBlock";
+import Disclaimer from "./components/Disclaimer";
+import Footer from "./components/Footer";
+import WhatIsNextSection from "./components/WhatIsNextSection";
+
+export default function Newsletter() {
+  return (
+    <Html>
+      <Head></Head>
+      <Preview>
+        Discover how Loop I is transforming stroke rehabilitation
+      </Preview>
+      <Body
+        style={{
+          margin: 0,
+          padding: 0,
+          fontFamily:
+            "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Arial, sans-serif",
+          lineHeight: "1.5",
+          fontSize: "17px",
+        }}
+      >
+        <Container
+          style={{
+            maxWidth: "700px",
+            margin: "0 auto",
+            padding: 0,
+            backgroundColor: "#fff",
+          }}
+        >
+          <TopBar />
+          <Logo width="337px" />
+          <Hero />
+          <VisionSection />
+          <SectionImage
+            src="/home/thesilouette/Documents/Neubond/reset-password-site/public/email/Newsletter-2/Team.jpg"
+            alt="User testing our Neubond device"
+          />
+          <WorkingOnSection />
+          <WhatIsNextSection />
+          <LookingAheadSection />
+          <SocialBlock />
+          <Footer />
+          <Disclaimer />
+        </Container>
+      </Body>
+    </Html>
+  );
+}
